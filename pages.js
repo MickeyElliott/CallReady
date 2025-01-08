ee496fa1377829d3d3862812f7df507d3df2ef9e
Mickey Elliott <mickeyelliott@hotmail.com>
2025-01-07 22:11:14 -0800

    Corrections

--- pages.js
+++ pages.js
@@ -60,7 +60,7 @@ const pages = [
           
               <div class="card-header">Clearing Codes</div>
             
-              <ul id=\"clearingCodes\" class=\"list-group list-group-flush\">
+              <ul id="clearingCodes" class="list-group list-group-flush">
                 
                 <li class="list-group-item d-flex justify-content-between"><strong>0</strong> No pursuit</li>
                 <li class="list-group-item d-flex justify-content-between"><strong>2</strong> Mental Illness</li>
@@ -114,38 +114,19 @@ const pages = [
         "title": "Evidence Guide", 
         "description": "A Referance guide for submitting evidence. - updated 1/07/25",
         "content":`
-
-        <div class="row">
-          <div class="col">
-            <ol class="list-group list-group-numbered">
-              <li class="list-group-item d-flex justify-content-between align-items-start">
-                <div class="ms-2 me-auto">
-                  <div class="fw-bold">Subheading</div>
-                  Content for list item
-                </div>
-                <span class="badge text-bg-primary rounded-pill">14</span>
-              </li>
-              <li class="list-group-item d-flex justify-content-between align-items-start">
-                <div class="ms-2 me-auto">
-                  <div class="fw-bold">Subheading</div>
-                  Content for list item
-                </div>
-                <span class="badge text-bg-primary rounded-pill">14</span>
-              </li>
-              <li class="list-group-item d-flex justify-content-between align-items-start">
-                <div class="ms-2 me-auto">
-                  <div class="fw-bold">Subheading</div>
-                  Content for list item
-                </div>
-                <span class="badge text-bg-primary rounded-pill">14</span>
-              </li>
-            </ol>
+          <div class="row">
+            <div class="col">
+              <ol class="list-group list-group-numbered">
+                <li class="list-group-item d-flex justify-content-between align-items-start">
+                  <div class="ms-2 me-auto">
+                    <div class="fw-bold">Returns for Correction</div>
+                    Blood kits and other evidence that belongs in a fridge are not stored in the lockers across the street. They are stored in the fridge acrose the street. The fridge is located through the MAIN entrance. (Not the door in the garage)
+                  </div>
+                </li>
+                
+              </ol>
+            </div>
           </div>
-        </div>
-        
-        
-        
-        
         `,
         "data":"{}"
        }
